19058a9f43f56ee749351cc6dd68954693fe2643
Lucas Barradas <95001458+b4rradas@users.noreply.github.com>
2024-10-30 17:14:03 -0300

    Add files via upload
    Técnica, desafio 2

---
+++ Tecnica - 2.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Informe a string para verificação da existência da letra a:");
+        string frase = Console.ReadLine();
+
+        int cont = 0;
+
+        foreach (char c in frase)
+        {
+            if (c == 'a' || c == 'A')
+            {
+                cont++;
+            }
+        }
+
+        if (cont > 0)
+        {
+            Console.WriteLine($"A letra 'a' aparece {cont} vezes na string");
+        }
+        else
+        {
+            Console.WriteLine("A letra 'a' não aparece nenhuma vez na string");
+        }
+    }
+}
